8d84316248df8537d1b2a0acfbc439f91a318ceb
Anton Soroko <anton.soroko@gmail.com>
2021-05-14 22:32:34 +0300

    Add assign torrent functionality (#13)
    * implement doAssign()
    
    * use doAssign()
    
    * update messages
    
    * add context_assign.py to addon.xml
    
    * added russian info to addon.xml + format xml

--- context_assign_season.py
+++ context_assign_season.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 
-from context.utils import doAssign
+from plugin import doAssignSeason
 
 if __name__ == '__main__':
-    doAssign()
+    doAssignSeason()
