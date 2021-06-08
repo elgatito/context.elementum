7c26b43d940de2fc8a2faeaa4f66e36f74a21327
Anton Soroko <anton.soroko@gmail.com>
2021-06-08 14:15:41 +0300

    add mediatype to context_menu.py

--- context_menu.py
+++ context_menu.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     truelabel = sys.listitem.getLabel()
     infolabel = xbmc.getInfoLabel('ListItem.Label')
     dbid = xbmc.getInfoLabel('ListItem.DBID')
+    mediatype = xbmc.getInfoLabel('ListItem.DBTYPE')
 
     log.info("truelabel: %s; infolabel: %s; dbid: %s; path: %s;" % (truelabel, infolabel, dbid, path))
-    xbmcgui.Dialog().notification("truelabel: %s" % truelabel, "infolabel: %s; dbid: %s; path: %s;" % (infolabel, dbid, path))
+    xbmcgui.Dialog().notification("truelabel: %s" % truelabel, "infolabel: %s; dbid: %s; mediatype: %s; path: %s;" % (infolabel, dbid, mediatype, path))
